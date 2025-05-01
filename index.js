1778d3961c599b619893b2af07610358bfdb7868
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-05-01 15:07:42 -0400

    seald-io/nedb

--- index.js
+++ index.js
@@ -1,71 +1,45 @@
 const express = require('express');
 // const Datastore = require('nedb');
-const loki = require('lokijs');
-const db = new loki('example.db', { autosave: true });
-
-let posts;
-
-db.loadDatabase({}, function (err) {
-  if (err) {
-    console.log('error : ' + err);
-  } else {
-    console.log('database loaded.');
-    prepare_database();
-  }
-});
-
-function prepare_database() {
-  posts = db.getCollection('posts');
-  if (!posts) {
-    // Create a collection
-    console.log('addCollection');
-    posts = db.addCollection('posts');
-  }
-}
+const Datastore = require('@seald-io/nedb');
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Starting server at ${port}`);
+app.listen(3000, () => {
+  console.log('listening at 3000');
+  console.log('open in browser http://localhost:3000/');
 });
-
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-// const database = new Datastore('database.db');
-// database.loadDatabase();
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 app.get('/api', (request, response) => {
-  if (posts) {
-    let data = posts.find();
-    // console.log('api data', data);
-    console.log('api data.length', data.length);
+  database.find({}, (err, data) => {
+    if (err) {
+      console.log('database.find err', err);
+      // console.log('database.find data', data);
+      response.end();
+      return;
+    }
+    // console.log('database.find data', data);
+    console.log('database.find data keys', Object.keys(data));
     response.json(data);
-  } else {
-    response.end();
-  }
+  });
 });
 
 app.post('/api', (request, response) => {
   const data = request.body;
   const timestamp = Date.now();
   data.timestamp = timestamp;
-  // database.insert(data);
-  // { lat, lon, mood, image64 }
-  // console.log('api insert', data);
-  // console.log('api insert data', data);
-  console.log('api insert data keys', Object.keys(data));
-  posts.insert(data);
+
+  // Insert the data into the database and check for errors
+  // console.log('database.insert data', data);
+  console.log('database.insert data', Object.keys(data));
+  database.insert(data, (err, newDoc) => {
+    console.log('database.insert err', err);
+    // console.log('database.insert newDoc', newDoc);
+    console.log('database.insert newDoc keys', Object.keys(newDoc));
+  });
+
   response.json(data);
 });
-
-// For local development
-// if (process.env.NODE_ENV !== 'production') {
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     console.log(`Server running in development mode on port ${PORT}`);
-//   });
-// }
-
-// For Vercel deployment
-// module.exports = app;
